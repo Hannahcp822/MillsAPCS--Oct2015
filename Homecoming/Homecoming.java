ed4112df2c693a1400fa7d0b66b1b69fd8aaaa87
Hannahcp822 <hannah123p@gmail.com>
2015-10-23 08:50:06 -0700

    10/23/15 - Homecoming

--- Homecoming/Homecoming.java
+++ Homecoming/Homecoming.java
@@ -3,54 +3,52 @@
  * Write a description of class Homecoming here.
  * 
  * @author Hannah Pang 
- * @version October 22, 2015
+ * @version October 23, 2015
  */
 
 import java.util.*;
 
 public class Homecoming
 {
-   Scanner userinput = new Scanner(System.in);
-   String myName = "Vickie Viking";
-   String theirName;
-   double ticketPrice; double flowerPrice; double picturePrice; double dinnerPrice; double clothesPrice; double miscPrice;
-   double subtotalPrice; double tax; double totalPrice;
-   
-   public void getADate()
+   public static void getADate(String myName)
    {
        System.out.println("Hi! My name is " + myName);
        System.out.print("What's your name? ");
-       theirName = userinput.nextLine();
+       String theirName = userinput.nextLine();
+       return theirName; 
        System.out.println("Nice to meet you " + theirName);
        System.out.print("Would you like to go to Homecoming with me? ");
        String answer = userinput.nextLine();
        System.out.println("Great! It'll be a magical night -- " + myName + " and " + theirName + " going to the Mills Homecoming Dance 2015!");
    }
    
-   public void damage()
+   public static void damage()
    {
        System.out.println("Let's figure out the damage for the evening.");
        System.out.println("What are your estimated costs?");
        System.out.print("Tickets (each): $");
-       ticketPrice = userinput.nextDouble();
+       double ticketPrice = userinput.nextDouble();
        System.out.print("Flowers (total for both of you): $");
-       flowerPrice = userinput.nextDouble();
+       double flowerPrice = userinput.nextDouble();
        System.out.print("Pictures: $");
-       picturePrice = userinput.nextDouble();
+       double picturePrice = userinput.nextDouble();
        System.out.print("Dinner (per meal): $");
-       dinnerPrice = userinput.nextDouble();
+       double dinnerPrice = userinput.nextDouble();
        System.out.print("New clothes (tie to match dress? Vest? Dress? Non-tennis shoes?): $");
-       clothesPrice = userinput.nextDouble();
+       double clothesPrice = userinput.nextDouble();
        System.out.print("Any other expenses (hair, makeup, limo, etc): $");
-       miscPrice = userinput.nextDouble();
-       subtotalPrice = ticketPrice + flowerPrice + picturePrice + dinnerPrice + clothesPrice + miscPrice;
-       tax = (dinnerPrice * 0.0825) + (clothesPrice * 0.0825) + (miscPrice * 0.0825);
-       totalPrice = subtotalPrice + tax;
+       double miscPrice = userinput.nextDouble();
+       double subtotalPrice = ticketPrice + flowerPrice + picturePrice + dinnerPrice + clothesPrice + miscPrice;
+       double tax = (dinnerPrice * 0.0825) + (clothesPrice * 0.0825) + (miscPrice * 0.0825);
+       double totalPrice = subtotalPrice + tax;
        System.out.println("So, you should expect to spend, including tax, $" + totalPrice);
        System.out.println("If you split the cost with your date, each will pay: $" + (totalPrice / 2));
+       System.out.println();
+       statsTable(theirName, ticketPrice, flowerPrice, picturePrice, dinnerPrice, clothesPrice, miscPrice, subtotalPrice, tax, totalPrice);
    }
    
-   public void statsTable()
+   public static void statsTable(String theirName, double ticketPrice, double flowerPrice, double picturePrie, double dinnerPrice, double clothesPrice, 
+                                 double miscPrice, double subtotalPrice, double tax, double totalPrice)
    {
        System.out.println("MHS Homecoming 2015");
        System.out.println("Vickie Viking and " + theirName);
@@ -69,12 +67,13 @@ public class Homecoming
    
    public static void main(String[] args)
    {
-       Homecoming H = new Homecoming();
-       H.getADate();
+       Scanner userinput = new Scanner(System.in);
+       String myName = "Vickie Viking";
+       getADate(userinput);
        System.out.println();
-       H.damage();
+       damage(userinput);
        System.out.println();
-       H.statsTable();
+       statsTable(userinput);
    }
 }
 
