eacd693d07f7170054cefb048ff5163f3fe8cb07
Hannahcp822 <hannahpang@Hannahs-Laptop.local>
2015-10-24 16:01:01 -0700

    10/24/15 - Homecoming

--- Homecoming/Homecoming.java
+++ Homecoming/Homecoming.java
@@ -3,27 +3,30 @@
  * Write a description of class Homecoming here.
  * 
  * @author Hannah Pang 
- * @version October 23, 2015
+ * @version October 25, 2015
  */
 
 import java.util.*;
 
 public class Homecoming
 {
-   public static void getADate(String myName)
+   // Exercise 1
+   public static String getADate(Scanner userinput, String myName)
    {
        System.out.println("Hi! My name is " + myName);
        System.out.print("What's your name? ");
-       String theirName = userinput.nextLine();
-       return theirName; 
+       String theirName = userinput.nextLine(); 
        System.out.println("Nice to meet you " + theirName);
        System.out.print("Would you like to go to Homecoming with me? ");
        String answer = userinput.nextLine();
        System.out.println("Great! It'll be a magical night -- " + myName + " and " + theirName + " going to the Mills Homecoming Dance 2015!");
+       return theirName;
    }
    
-   public static void damage()
+   // Exercise 2
+   public static void damage(Scanner userinput, String theirName)
    {
+       System.out.println();
        System.out.println("Let's figure out the damage for the evening.");
        System.out.println("What are your estimated costs?");
        System.out.print("Tickets (each): $");
@@ -40,14 +43,16 @@ public class Homecoming
        double miscPrice = userinput.nextDouble();
        double subtotalPrice = ticketPrice + flowerPrice + picturePrice + dinnerPrice + clothesPrice + miscPrice;
        double tax = (dinnerPrice * 0.0825) + (clothesPrice * 0.0825) + (miscPrice * 0.0825);
+       tax = (double)Math.round(tax*100)/100;
        double totalPrice = subtotalPrice + tax;
        System.out.println("So, you should expect to spend, including tax, $" + totalPrice);
        System.out.println("If you split the cost with your date, each will pay: $" + (totalPrice / 2));
-       System.out.println();
-       statsTable(theirName, ticketPrice, flowerPrice, picturePrice, dinnerPrice, clothesPrice, miscPrice, subtotalPrice, tax, totalPrice);
+       System.out.println();  
+       statsTable(userinput, theirName, ticketPrice, flowerPrice, picturePrice, dinnerPrice, clothesPrice, miscPrice, subtotalPrice, tax, totalPrice);
    }
    
-   public static void statsTable(String theirName, double ticketPrice, double flowerPrice, double picturePrie, double dinnerPrice, double clothesPrice, 
+   // Exercise 3
+   public static void statsTable(Scanner userinput, String theirName, double ticketPrice, double flowerPrice, double picturePrice, double dinnerPrice, double clothesPrice, 
                                  double miscPrice, double subtotalPrice, double tax, double totalPrice)
    {
        System.out.println("MHS Homecoming 2015");
@@ -67,13 +72,10 @@ public class Homecoming
    
    public static void main(String[] args)
    {
-       Scanner userinput = new Scanner(System.in);
-       String myName = "Vickie Viking";
-       getADate(userinput);
-       System.out.println();
-       damage(userinput);
-       System.out.println();
-       statsTable(userinput);
+       Scanner userinput = new Scanner(System.in); // instantiates Scanner
+       final String myName = "Vickie Viking"; // defines constant string
+       final String theirName = getADate(userinput, myName);
+       damage(userinput, theirName);
    }
 }
 
